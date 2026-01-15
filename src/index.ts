aa75a563213875025e3b1a33c9caa66ec2545a1d
Irham Sahbana <irhamsahbana@gmail.com>
2026-01-16 03:18:32 +0800

    refactor: simplify app initialization by direct import
    Remove dynamic import and type casting in favor of direct import

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
+import App from '@/bin/app'
 import { startTelemetry } from '@/telemetry'
 
-type AppConstructor = typeof import('./bin/app').default
-
 startTelemetry()
-void import('./bin/app.js').then((mod) => {
-  const App = mod.default as unknown as AppConstructor
-  new App()
-})
+new App()
