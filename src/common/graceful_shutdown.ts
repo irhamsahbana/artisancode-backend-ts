6c3064980d136862adcb32a4396a9ea9565ef826
Irham Sahbana <irhamsahbana@gmail.com>
2026-01-16 16:06:50 +0800

    feat(graceful_shutdown): add telemetry shutdown to graceful shutdown process

--- src/common/graceful_shutdown.ts
+++ src/common/graceful_shutdown.ts
@@ -2,16 +2,23 @@ import http from 'http'
 
 import prisma from '@/common/prisma'
 import logger from '@/config/logger'
+import { shutdownTelemetry } from '@/telemetry'
 
 const shutdown = (server: http.Server) => {
   logger.info('Received kill signal, shutting down gracefully ...')
 
   server.close(async () => {
     logger.info('HTTP server closed')
+
     // Here you will close other connections such as Database
     logger.info('Disconnecting from Prisma ...')
     await prisma.$disconnect()
     logger.info('Prisma disconnected')
+
+    logger.info('Shutting down telemetry ...')
+    await shutdownTelemetry()
+    logger.info('Telemetry shut down')
+
     process.exit(0)
   })
 
