ca045118e0b80ae7232af3908a14afef845b6475
Irham Sahbana <irhamsahbana@gmail.com>
2026-06-16 20:40:31 +0700

    refactor(resilience): update error classes to use httpCode
    - CircuitBreakerError and ResilienceExhaustedError now use httpCode

--- src/common/packages/resilience/errors.ts
+++ src/common/packages/resilience/errors.ts
@@ -2,14 +2,14 @@ import { AppError, ErrorCode } from '@/common/packages/types'
 
 export class CircuitBreakerError extends AppError {
   constructor(message = 'Service unavailable due to open circuit breaker') {
-    super(ErrorCode.CIRCUIT_BREAKER_OPEN, message, { statusCode: 503 })
+    super(ErrorCode.CIRCUIT_BREAKER_OPEN, message, { httpCode: 503 })
     this.name = 'CircuitBreakerError'
   }
 }
 
 export class ResilienceExhaustedError extends AppError {
   constructor(message = 'All retry attempts exhausted') {
-    super(ErrorCode.RESILIENCE_EXHAUSTED, message, { statusCode: 503 })
+    super(ErrorCode.RESILIENCE_EXHAUSTED, message, { httpCode: 503 })
     this.name = 'ResilienceExhaustedError'
   }
 }
